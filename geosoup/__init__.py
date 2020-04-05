c4d7a197ade77baa0334db88694b73bd732f52bb
Richard Massey <rm885@nau.edu>
2020-04-05 00:17:11 -0700

    Update __init__.py

--- geosoup/__init__.py
+++ geosoup/__init__.py
@@ -1,16 +1,8 @@
-from geosoup.common import Sublist, Handler, FTPHandler, Opt
-from geosoup.gdaldefs import OGR_GEOM_DEF, OGR_TYPE_DEF, OGR_FIELD_DEF, \
-    GDAL_FIELD_DEF, OGR_FIELD_DEF_INV, GDAL_FIELD_DEF_INV
-from geosoup.regression import RFRegressor, MRegressor, HRFRegressor, _Regressor
+from geosoup.common import Sublist, Handler, Opt, FTPHandler, Logger, Timer
+from geosoup.regression import RFRegressor, MRegressor, HRFRegressor
 from geosoup.samples import Samples
-from geosoup.raster import Raster, MultiRaster
-from geosoup.timer import Timer
+from geosoup.raster import Raster, MultiRaster, Terrain, GDAL_FIELD_DEF, GDAL_FIELD_DEF_INV
 from geosoup.distance import Mahalanobis, Distance, Euclidean
 from geosoup.exceptions import ObjectNotFound, UninitializedError, FieldError, \
     FileNotFound, TileNotFound, ImageProcessingError
-from geosoup.vector import Vector
-from geosoup.logger import Logger
-
-
-
-
+from geosoup.vector import Vector, OGR_GEOM_DEF, OGR_TYPE_DEF, OGR_FIELD_DEF, OGR_FIELD_DEF_INV
