03dae9192a87b2b5d1ed409c0620109d823d8371
Richard Massey <rm885@nau.edu>
2020-10-18 15:21:17 -0700

    updated __init__.py

--- geosoup/__init__.py
+++ geosoup/__init__.py
@@ -3,4 +3,4 @@ from geosoup.raster import Raster, MultiRaster, GDAL_FIELD_DEF, GDAL_FIELD_DEF_I
 from geosoup.terrain import Terrain
 from geosoup.exceptions import ObjectNotFound, UninitializedError, FieldError, \
     FileNotFound, TileNotFound, ImageProcessingError
-from geosoup.vector import Vector, OGR_GEOM_DEF, OGR_TYPE_DEF, OGR_FIELD_DEF, OGR_FIELD_DEF_INV
+from geosoup.vector import Vector, OGR_GEOM_DEF, OGR_TYPE_DEF, OGR_FIELD_DEF
