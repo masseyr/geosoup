906b438b211c386baa77d497ba690880f7b86942
Richard Massey <rm885@nau.edu>
2020-10-20 15:22:02 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="geosoup",
-    version="0.1.37",
+    version="0.1.62",
     author="Richard Massey",
     author_email="rm885@nau.edu",
     license='Apache License 2.0',
@@ -19,21 +19,13 @@ setuptools.setup(
         'Intended Audience :: Science/Research',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-
-
     ],
     install_requires=[
         'psutil',
-        'h5py',
         'numpy',
-        'scikit-learn',
-        'scipy',
     ],
     keywords='geospatial raster vector global spatial regression hierarchical samples random',
 )
